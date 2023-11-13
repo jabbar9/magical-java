10509806796b06f839dcd02845a29c295a026c4a
Sanskruti Chavan <sanskrutichavandev@gmail.com>
2023-11-13 22:28:46 +0530

    Signup Featuress added and login page created successfully
    Few bugs solved and contact page added.

---
+++ menubar.js
@@ -0,0 +1,12 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  
